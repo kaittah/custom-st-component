708e733284d5db7bc6e20c6d92fff5f14e521e2b
kaittah <kaitlynhennacy@gmail.com>
2024-05-19 23:10:51 -0700

    Add cards for rendering

--- template/my_component/frontend/src/index.tsx
+++ template/my_component/frontend/src/index.tsx
@@ -2,9 +2,25 @@ import React from "react"
 import ReactDOM from "react-dom"
 import MyComponent from "./MyComponent"
 
+import { ThemeProvider, createTheme, lightThemePrimitives } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+
+const engine = new Styletron();
+
+const theme = createTheme({
+  ...lightThemePrimitives,
+  primaryFontFamily: "Arial",
+});
+
 ReactDOM.render(
   <React.StrictMode>
+  <StyletronProvider value={engine}>
+    <ThemeProvider theme={theme}>
     <MyComponent />
+  </ThemeProvider>
+  </StyletronProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
